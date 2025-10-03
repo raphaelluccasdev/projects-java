ec028e28ae596d129b874ec17005d00cd3f97e78
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-10-03 09:22:44 +0000

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/springproject/course/entities/User.java
+++ src/main/java/com/springproject/course/entities/User.java
@@ -3,6 +3,8 @@ package com.springproject.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -79,5 +84,9 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, email, phone, password);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
