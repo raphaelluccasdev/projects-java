e7a0c88fd544c056c2ffa4a681268936cf9d67d5
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-10-06 10:32:49 +0000

    Category

--- src/main/java/com/springproject/course/config/TestConfig.java
+++ src/main/java/com/springproject/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.springproject.course.config;
 
+import com.springproject.course.entities.Category;
 import com.springproject.course.entities.Order;
 import com.springproject.course.entities.User;
 import com.springproject.course.entities.enums.OrderStatus;
+import com.springproject.course.repositories.CategoryRepository;
 import com.springproject.course.repositories.OrderRepository;
 import com.springproject.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
